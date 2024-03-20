e9151e6d4320ed300a8edf67938cce3c2de43dac
RajKumar Gogu <rajkumar.gogu@betalen.in>
2024-03-20 16:16:46 +0530

    Add project files.

---
+++ Aggregate.Intellegence.Library.Web.UI/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Aggregate.Intellegence.Library.Web.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Aggregate.Intellegence.Library.Web.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Aggregate.Intellegence.Library.Web.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Aggregate.Intellegence.Library.Web.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
